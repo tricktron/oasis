311af74ce69fcfed8850aa3652a329849f0f0033
Thibault Gagnaux <tgagnaux@gmail.com>
2022-10-04 16:28:31 +0200

    refactor: remove unneeded pkgs-fork again

--- flake.nix
+++ flake.nix
@@ -7,18 +7,16 @@
         [ "tricktron.cachix.org-1:N1aBeQuELyEAOgvizaDC/qqFltwv7N7oSMaNozyDz6w=" ];
     };
     inputs.nixpkgs.url      = "github:NixOS/nixpkgs/release-22.05";
-    inputs.nixpkgs-fork.url = "github:tricktron/nixpkgs/develop";
     inputs.flake-utils.url  = "github:numtide/flake-utils";
 
 
-    outputs = { self, nixpkgs, nixpkgs-fork, flake-utils }:
+    outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ]
     (
         system:
         let
             pkgs       = nixpkgs.legacyPackages.${system}.pkgs;
             pkgs-host  = pkgs.pkgsCross.aarch64-multiplatform-musl;
-            pkgs-fork  = nixpkgs-fork.legacyPackages.${system}.pkgs;
             oasis-deps = with pkgs;
             [
                 bison
@@ -29,7 +27,8 @@
                 tzdata.bin
                 wayland
                 nasm
-            ] ++ [ pkgs-fork.pax ];
+                pax
+            ];
         in
         {
             devShells.default = pkgs-host.mkShell 
