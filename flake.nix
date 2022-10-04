043b8a43ac1ae72ae4648377d58d1931d7ee4150
Thibault Gagnaux <tgagnaux@gmail.com>
2022-10-04 22:58:22 +0200

    feat: update flake to latest linux version

--- flake.nix
+++ flake.nix
@@ -1,22 +1,24 @@
 {
     description             = "oasis-dev-shell";
-    nixConfig              =
+    nixConfig               =
     {
         extra-sbustituters        = [ "https://tricktron.cachix.org" ];
         extra-trusted-public-keys = 
         [ "tricktron.cachix.org-1:N1aBeQuELyEAOgvizaDC/qqFltwv7N7oSMaNozyDz6w=" ];
     };
     inputs.nixpkgs.url      = "github:NixOS/nixpkgs/release-22.05";
+    inputs.nixpkgs-fork.url = "github:tricktron/nixpkgs/develop";
     inputs.flake-utils.url  = "github:numtide/flake-utils";
 
 
-    outputs = { self, nixpkgs, flake-utils }:
+    outputs                 = { self, nixpkgs, nixpkgs-fork, flake-utils }:
     flake-utils.lib.eachSystem [ "aarch64-linux" "x86_64-linux" ]
     (
         system:
         let
             pkgs       = nixpkgs.legacyPackages.${system}.pkgs;
-            pkgs-host  = pkgs.pkgsCross.aarch64-multiplatform-musl;
+	        pkgs-host  = pkgs.pkgsCross.aarch64-multiplatform-musl;
+            pkgs-fork  = nixpkgs-fork.legacyPackages.${system}.pkgs;
             oasis-deps = with pkgs;
             [
                 bison
@@ -25,12 +27,29 @@
                 curl
                 tzdata.out
                 tzdata.bin
-                wayland
-                nasm
+		        wayland
+		        nasm
                 pax
             ];
         in
         {
+	        defaultPackage    = pkgs.symlinkJoin
+            {
+                name  = "oasis-live-cd";
+                paths =
+                [
+                    pkgs-host.pkgsStatic.linuxKernel.kernels.linux_zen
+	                pkgs-host.pkgsStatic.bashInteractive
+	                pkgs-host.pkgsStatic.squashfsTools
+	                pkgs-host.pkgsStatic.zip
+                    pkgs-host.pkgsStatic.gnugrep
+                    pkgs-host.pkgsStatic.findutils
+                    pkgs-host.pkgsStatic.kmod
+                    pkgs-host.pkgsStatic.coreutils
+	                pkgs-fork.pkgsCross.musl64.pkgsStatic.cdrkit
+                ];
+            };
+
             devShells.default = pkgs-host.mkShell 
             {
                 nativeBuildInputs = oasis-deps;
